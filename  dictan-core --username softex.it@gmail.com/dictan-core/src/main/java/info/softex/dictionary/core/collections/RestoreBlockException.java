8d8627cfe261cac02387c4b417f5d24629f838cc
softex.it@gmail.com <softex.it@gmail.com@a6764a1d-cc78-86b3-96da-d690b8e69a50>
2012-06-04 01:57:39 +0000

    Update to version 3.2
    git-svn-id: https://dictan-core.googlecode.com/svn/trunk@15 a6764a1d-cc78-86b3-96da-d690b8e69a50

---  dictan-core --username softex.it@gmail.com/dictan-core/src/main/java/info/softex/dictionary/core/collections/RestoreBlockException.java
+++  dictan-core --username softex.it@gmail.com/dictan-core/src/main/java/info/softex/dictionary/core/collections/RestoreBlockException.java
@@ -1,7 +1,7 @@
 /*
  *  Dictan Open Dictionary Java Library presents the core interface and functionality for dictionaries. 
  *	
- *  Copyright (C) 2011 - 2012  Dmitry Viktorov <dmitry.viktorov@softex.info> <http://www.softex.info>
+ *  Copyright (C) 2010 - 2012  Dmitry Viktorov <dmitry.viktorov@softex.info> <http://www.softex.info>
  *	
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License (LGPL) as 
