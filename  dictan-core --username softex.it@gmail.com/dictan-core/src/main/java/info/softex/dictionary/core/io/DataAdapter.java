fd7a4504f4321a6da2beef583f1ec7d45f6a1a82
softex.it <softex.it@a6764a1d-cc78-86b3-96da-d690b8e69a50>
2012-10-17 18:05:00 +0000

    Version 3.5
    git-svn-id: https://dictan-core.googlecode.com/svn/trunk@21 a6764a1d-cc78-86b3-96da-d690b8e69a50

---
+++  dictan-core --username softex.it@gmail.com/dictan-core/src/main/java/info/softex/dictionary/core/io/DataAdapter.java
@@ -0,0 +1,90 @@
+/*
+ *  Dictan Open Dictionary Java Library presents the core interface and functionality for dictionaries. 
+ *	
+ *  Copyright (C) 2010 - 2012  Dmitry Viktorov <dmitry.viktorov@softex.info> <http://www.softex.info>
+ *	
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License (LGPL) as 
+ *  published by the Free Software Foundation, either version 3 of the License, 
+ *  or any later version.
+ *	
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *	
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.softex.dictionary.core.io;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * 
+ * @since version 2.5, 06/30/2011
+ * 
+ * @modified version 3.4, 07/07/2012
+ * 
+ * @author Dmitry Viktorov
+ *
+ */
+public class DataAdapter {
+	
+	private final InputStream is; 
+	
+	public DataAdapter(File file) throws IOException {
+		if (file.length() > Integer.MAX_VALUE) {
+            throw new IOException("File is too large!");
+        }
+		this.is = new FileInputStream(file);
+	}
+	
+	public DataAdapter(InputStream is) {
+		this.is = is;
+	}
+	
+	public File toFile(File tempDir, String name, String prefix) throws IOException {
+		
+		prefix = (prefix == null ? "": prefix);
+		
+		byte buffer[] = new byte[16384];
+		
+		File file = new File(tempDir.getAbsolutePath() + File.separator + prefix + name);
+		
+	    FileOutputStream fos = new FileOutputStream(file);
+	    int len = 0;
+	    while ((len = is.read(buffer)) >= 0) {
+	    	fos.write(buffer,0,len);
+	    }
+	    
+	    fos.close();
+	    is.close();
+		
+		return file;
+	}
+	
+	public byte[] toByteArray() throws IOException {
+		
+		byte buffer[] = new byte[16384];
+
+		int nRead;
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		while ((nRead = is.read(buffer, 0, buffer.length)) >= 0) {
+			baos.write(buffer, 0, nRead);
+		}
+
+		baos.close();
+		is.close();
+
+		return baos.toByteArray();
+
+	}
+
+}
---
+++  dictan-core --username softex.it@gmail.com/dictan-core/src/main/java/info/softex/dictionary/core/io/SmartInflaterInputStream.java
@@ -0,0 +1,87 @@
+/*
+ *  Dictan Open Dictionary Java Library presents the core interface and functionality for dictionaries. 
+ *	
+ *  Copyright (C) 2010 - 2012  Dmitry Viktorov <dmitry.viktorov@softex.info> <http://www.softex.info>
+ *	
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License (LGPL) as 
+ *  published by the Free Software Foundation, either version 3 of the License, 
+ *  or any later version.
+ *	
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *	
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.softex.dictionary.core.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+import java.util.zip.Inflater;
+import java.util.zip.InflaterInputStream;
+
+/**
+ * 
+ * The InflaterInputStream implementation which is reliable in terms 
+ * of skipping and reading the data bytes. The class also presents 
+ * convenience methods for reading data.
+ * 
+ * @since version 1.2, 09/28/2010
+ *
+ * @modified version 2.6, 09/24/2011
+ * 
+ * @author Dmitry Viktorov
+ * 
+ */
+public class SmartInflaterInputStream extends InflaterInputStream {
+	
+	protected long totalBytesPassed;
+	
+	public SmartInflaterInputStream(InputStream is) {
+		super(is);
+	}
+
+	public SmartInflaterInputStream(InputStream is, Inflater inflater) {
+		super(is, inflater);
+	}
+	
+	public SmartInflaterInputStream(InputStream is, Inflater inflater, int bufferSize) {
+		super(is, inflater, bufferSize);
+	}
+		
+	/**
+	 * skip() and read() methods call this method, so the passed bytes 
+	 * counter is incremented here. There is also a loop to make sure that 
+	 * all the maximum possible requested bytes read.
+	 */
+	@Override 
+	public int read(byte[] buffer, int offset, int length) throws IOException {
+		int read = 0;
+		while(read < length && super.available() > 0) {
+			int curRead = super.read(buffer, read, length - read);
+			if (curRead >= 0) {
+				read += curRead;
+				this.totalBytesPassed += curRead;
+			}
+		}
+		return read;
+	}
+	
+	public int readInt() throws IOException {
+		byte[] intData = new byte[4];
+		if (read(intData, 0, intData.length) < 4) {
+			throw new IOException("Couldn't read int: no enough bytes available");
+		}
+		return ByteBuffer.wrap(intData).getInt();
+	}
+	
+	public long getTotalBytesPassed() {
+		return this.totalBytesPassed;
+	}
+
+}
