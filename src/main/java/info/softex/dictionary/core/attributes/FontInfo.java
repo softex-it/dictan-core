8151177c954876e9ff51530c90f4bc4c112ef8b4
Dmitry Viktorov <softex.it@gmail.com>
2014-02-20 19:58:40 -0800

    Added FontInfo

---
+++ src/main/java/info/softex/dictionary/core/attributes/FontInfo.java
@@ -0,0 +1,84 @@
+/*
+ *  Dictan Open Dictionary Java Library presents the core interface and functionality for dictionaries. 
+ *	
+ *  Copyright (C) 2010 - 2014  Dmitry Viktorov <dmitry.viktorov@softex.info> <http://www.softex.info>
+ *	
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License (LGPL) as 
+ *  published by the Free Software Foundation, either version 3 of the License, 
+ *  or any later version.
+ *	
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Lesser General Public License for more details.
+ *	
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package info.softex.dictionary.core.attributes;
+
+/**
+ * 
+ * @since version 4.1, 02/20/2014
+ * 
+ * @author Dmitry Viktorov
+ *
+ */
+public class FontInfo {
+	
+	private String name = null;
+	
+	private String style = null;
+	
+	private String filePath = null;
+	
+	private int size;
+	
+	public FontInfo() {}
+	
+	public FontInfo(String inFontName, String inFontStyle, int inFontSize) {
+		this.name = inFontName;
+		this.style = inFontStyle;
+		this.size = inFontSize;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStyle() {
+		return style;
+	}
+
+	public void setStyle(String style) {
+		this.style = style;
+	}
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + name + ", Style: " + style + ", Size: " + size + ", File Path: " + filePath;
+	}
+
+}
