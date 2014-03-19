00b08aabbf8a81b2d060649cc770466a43ec98ae
Dmitry Viktorov <softex.it@gmail.com>
2014-03-19 09:07:39 -0700

    Fixed compatibility of the native sqlite lib name in Mac at Java 7

--- src/main/java/info/softex/dictionary/core/database/BasicSQLiteConnectionFactory.java
+++ src/main/java/info/softex/dictionary/core/database/BasicSQLiteConnectionFactory.java
@@ -25,18 +25,28 @@ import java.sql.SQLException;
 import java.util.Map;
 import java.util.Properties;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * 
  * @since version 2.6, 09/07/2011
  * 
  * @modified version 3.9, 12/03/2013
+ * @modified version 4.4, 03/19/2014
  * 
  * @author Dmitry Viktorov
  * 
  */
 public class BasicSQLiteConnectionFactory implements DatabaseConnectionFactory {
 	
+	private static final Logger log = LoggerFactory.getLogger(BasicSQLiteConnectionFactory.class.getSimpleName());
+	
 	protected static final String DEFAULT_JDBC_DRIVER = "org.sqlite.JDBC";
+	
+	protected static final String SYSPROP_SQLITE_LIB_NAME_KEY = "org.sqlite.lib.name";
+	protected static final String SYSPROP_SQLITE_LIB_NAME_VALUE = "sqlitejdbc";
+	protected static final String SYSPROP_SQLITE_LIB_NAME_EXT = ".jnilib";
 	
 	/**
 	 * 
@@ -61,8 +71,24 @@ public class BasicSQLiteConnectionFactory implements DatabaseConnectionFactory {
 
 		// Load the sqlite-JDBC driver using the current class loader
 		try {
+			
 			Class.forName(driverClassName);
+			
+			// Fix the change at Java 7 at OS X where mapping to .jnilib was changed to .dylib
+			String sqliteFullLibName = System.mapLibraryName(SYSPROP_SQLITE_LIB_NAME_VALUE);
+			log.debug("SQLite native library name: {}", sqliteFullLibName);
+			
+			if (sqliteFullLibName != null && !sqliteFullLibName.endsWith(SYSPROP_SQLITE_LIB_NAME_EXT)) {
+				int dotInd = sqliteFullLibName.lastIndexOf(".");
+				if (dotInd > 0) {
+					sqliteFullLibName = sqliteFullLibName.substring(0, dotInd) + SYSPROP_SQLITE_LIB_NAME_EXT;
+					log.info("SQLite native library name changed to: {}", sqliteFullLibName);
+					System.setProperty(SYSPROP_SQLITE_LIB_NAME_KEY, sqliteFullLibName);
+				}
+			}
+			
 		} catch (ClassNotFoundException e) {
+			log.error("Error", e);
 			throw new SQLException("Couldn't find the JDBC Driver: " + driverClassName + "; Reason: " + e.getMessage());
 		}
 
